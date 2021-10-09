3657f888eedc6fc08dde862eae67a8cfd92cf8f7
Marco Briatore <30911931+MarcoBriatore@users.noreply.github.com>
2021-10-09 14:59:50 -0300

    Added functions IncreaseHealth, DecreaseHealth & Movement

---
+++ Game Mechanics/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float health = 100;
+    public Vector3 direction = Vector3.left;
+    public float velocity = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    public void IncreaseHealth(float amount)
+    {
+        this.health += Math.Abs(amount);
+    }
+
+    public void DecreaseHealth(float damage)
+    {
+        this.health -= -Math.Abs(damage);
+    }
+
+    public void Movement()
+    {
+        Vector3 direction = this.direction.normalized;
+        this.transform.position += direction * (velocity * Time.deltaTime);
+    }
+}
